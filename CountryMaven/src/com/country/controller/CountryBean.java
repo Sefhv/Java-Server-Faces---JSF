c58bae6099ae4f01633900b35551be544bc49c21
Sefhv <43103433+Sefhv@users.noreply.github.com>
2019-08-07 17:47:54 -0500

    Country Maven version 1.2

--- CountryMaven/src/com/country/controller/CountryBean.java
+++ CountryMaven/src/com/country/controller/CountryBean.java
@@ -20,14 +20,23 @@ public class CountryBean implements Serializable {
 	private List<Country> countryList;
 	private String statusText = "";
 
+	private boolean showList;
+	private boolean showCreate;
+	private boolean showRead;
+	private boolean showEdit;
+
 	public CountryBean() {
+		init();
+		readCountry();
+	}
+
+	public void init() {
 		country = new Country();
 		countryList = new ArrayList<>(1);
-		readCountry();
-//		countryList.add(new Country(1, "Colombia", "El mejor de todos", 1352, 1324, "No aplica"));
-//		countryList.add(new Country(8, "Argentina", "El mejor de todos", 1352, 1324, "No aplica"));
-//		countryList.add(new Country(4, "Brasil", "El mejor de todos", 1352, 1324, "No aplica"));
-//		countryList.add(new Country(7, "Uruguay", "El mejor de todos", 1352, 1324, "No aplica"));
+		showList = true;
+		showCreate = false;
+		showRead = false;
+		showEdit = false;
 	}
 
 	public void readCountry() {
@@ -38,64 +47,82 @@ public class CountryBean implements Serializable {
 		}
 	}
 
-	public String saveCountry() {
+	public void saveCountry() {
 		try {
 			CountryDao.addCountry(country);
 			countryList.add(country);
-			statusText = "El pais ha sido guardado correctamente";
-			return "index";
+			showList = true;
+			showCreate = false;
+			showRead = false;
+			showEdit = false;
+			statusText = "El pais ha sido creado correctamente";
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw e;
 		}
 	}
-
-	public String editCountry() {
+	
+	public void editCountry() {
 		try {
 			CountryDao.editCountry(country);
-			statusText = "El pais ha sido editado correctamente";
-			return "index";
+			showList = true;
+			showCreate = false;
+			showRead = false;
+			showEdit = false;
+			statusText = "El pais ha sido editado correctamente";			
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw e;
 		}
 	}
 
-	public String goAddCountry() {
+	
+	public void goAddCountry() {
+		showList = false;
+		showCreate = true;
+		showRead = false;
+		showEdit = false;
 		country = new Country();
-		return "create";
 	}
 
-	public String goEditCountry(Country c) {
+	public void goEditCountry(Country c) {
+		showList = false;
+		showCreate = false;
+		showRead = false;
+		showEdit = true;
 		country = c;
-		return "edit";
 	}
 
-	public String goShowCountry(Country c) {
+	public void goShowCountry(Country c) {
 		try {
+			showList = false;
+			showCreate = false;
+			showRead = true;
+			showEdit = false;
 			country = c;
-			return "read";
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw e;
-		}		
+		}
 	}
 
-	public String goDeleteCountry(Country c) {
+	public void goDeleteCountry(Country c) {
 		try {
 			CountryDao.deleteCountry(c);
 			countryList.remove(c);
 			statusText = "El pais ha sido eliminado correctamente";
-			return "index";
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw e;
 		}
 	}
 
-	public String goIndex() {
+	public void goIndex() {
 		statusText = "";
-		return "index";
+		showList = true;
+		showCreate = false;
+		showRead = false;
+		showEdit = false;
 	}
 
 	/**
@@ -124,5 +151,37 @@ public class CountryBean implements Serializable {
 
 	public void setStatusText(String statusText) {
 		this.statusText = statusText;
+	}
+
+	public boolean isShowList() {
+		return showList;
+	}
+
+	public void setShowList(boolean showList) {
+		this.showList = showList;
+	}
+
+	public boolean isShowCreate() {
+		return showCreate;
+	}
+
+	public void setShowCreate(boolean showCreate) {
+		this.showCreate = showCreate;
+	}
+
+	public boolean isShowEdit() {
+		return showEdit;
+	}
+
+	public void setShowEdit(boolean showEdit) {
+		this.showEdit = showEdit;
+	}
+
+	public boolean isShowRead() {
+		return showRead;
+	}
+
+	public void setShowRead(boolean showRead) {
+		this.showRead = showRead;
 	}
 }
